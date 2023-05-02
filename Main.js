e8cb1bbe3f657d2157723852382005e604b7c1dc
SIBUSISOMT <sbusisozamowakhe18@gmail.com>
2023-05-02 04:34:11 +0200

    Added functionality.
    To-do list complete

--- Main.js
+++ Main.js
@@ -2,12 +2,24 @@
 const input_field = document.getElementById("new-task-input");
 const submit_button= document.getElementById("new-task-submit");
 const output = document.getElementById("List");
+const trash = document.getElementsByClassName("uil uil-airplay");
    
 
 submit_button.addEventListener('click',(e)=>{
-    e.preventDefault()
+ e.preventDefault()
  let list = document.createElement('p')
  list.innerText= input_field.value;
  input_field.value = '';
  output.appendChild(list)
+
+list.addEventListener('click',(e)=>{
+    list.style.textDecorationColor= 'orange'
+    list.style.textDecorationLine= 'line-through'
+
+
+})
+list.addEventListener('dblclick',()=>{
+    output.removeChild(list);
+})
+
 });
