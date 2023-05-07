feb07776c653a299cba3fdc467eab7f682d35f0d
SIBUSISOMT <sbusisozamowakhe18@gmail.com>
2023-05-07 12:02:21 +0200

    New updated To-Do List

--- Main.js
+++ Main.js
@@ -2,28 +2,36 @@
 const input_field = document.getElementById("new-task-input");
 const submit_button= document.getElementById("new-task-submit");
 const output = document.getElementById("List");
-const trash = document.getElementsByClassName("uil uil-airplay");
-const date = document.getElementsByClassName("date")
-   
+const setDate = document.getElementById("date");
+const spacing=document.getElementById('space');
+
 
 submit_button.addEventListener('click',(e)=>{
  e.preventDefault()
- let list = document.createElement('li')
+if (input_field.value.length == 0){
+    alert("Please input task");
+    
+} else {
+     
+ let list = document.createElement('p')
  list.innerText= input_field.value;
- input_field.value = '';
+ input_field.value = ''; 
  output.appendChild(list);
 
 list.addEventListener('click',(e)=>{
     list.style.textDecorationColor= 'orange'
     list.style.textDecorationLine= 'line-through'
-
-
 })
 list.addEventListener('dblclick',()=>{
     output.removeChild(list);
 })
+    
+    let dateAndTime = setDate.value;
+    list.innerHTML +="    "+"       [ "+dateAndTime+ " ]";
+   
+
+   
 
 
-
-});
+}});
  
